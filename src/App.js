8ac8651c2b2cead8ebefe8b4ce7ecf241cc1c346
amogh0503 <amoghagarwal0503@gmail.com>
2025-06-14 17:03:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Component } from 'react';
+
+export default class App extends Component {  
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+        <p>React is a JavaScript library for building user interfaces.</p>
+      </div>
+    );
+  }
+}
